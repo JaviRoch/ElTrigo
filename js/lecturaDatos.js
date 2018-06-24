7e58028e17034facb5f72b900d1a5efd9f55e93d
Unknown <javier.roche.m@gmail.com>
2018-06-24 12:02:14 +0200

    Implementada lectura de JSON y guardado en variable

--- js/lecturaDatos.js
+++ js/lecturaDatos.js
@@ -1,26 +1,41 @@
 
-//Transferencia des datos desde la base de datos JSON
-var xmlhttp = new XMLHttpRequest();
-var url = "/data.json";
-var datos;
-var libros;
-var ultimoLibro
-var miLibro;
-
-//Función al finalizar operación. Status a 200 ok, status a 404 fail
-xmlhttp.onreadystatechange = function(){
-	if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
-		//guardamos los datos en una variable
-		datos = xmlhttp.responseText;
-		console.log(datos);
-		//parseamos los datos de JSON a array
-		libros = JSON.parse(datos);
-		console.log("libros " + libros[0].edad);
-		ultimoLibro = libros.length;
-		console.log("Último libro creado " + ultimoLibro);
-	}
+// Transferencia des datos desde la base de datos JSON
+var xmlhttp = new XMLHttpRequest()
+var url = "/data.json"
+// Función al finalizar operación. Status a 200 ok, status a 404 fail
+xmlhttp.onreadystatechange = function () {
+	if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+		// Guardamos los datos en una variable
+		datos = xmlhttp.responseText
+		// Parseamos los datos de JSON a array
+		parseo(datos)
+}
 }
 
-//comunicación con el archivo JSON
-xmlhttp.open("GET",url,true);
-xmlhttp.send();
+// Comunicación con el archivo JSON
+xmlhttp.open("GET", url, true)
+xmlhttp.send()
+
+// Parseamos los datos desde JSON y guardamos el último libro en una variable
+function parseo (dat) {
+	var libro = [];
+  var libros = JSON.parse(dat)
+  var ultimoLibro = libros.length - 1
+	libro.push(ultimoLibro);
+	var edad = libros[ultimoLibro].edad
+	libro.push(edad);
+	var genero = libros[ultimoLibro].genero
+	libro.push(genero);
+	var nacimiento = libros[ultimoLibro].nacimiento
+	libro.push(nacimiento);
+	var laboral = libros[ultimoLibro].laboral
+	libro.push(laboral);
+
+// Iniciamos función de tratamiento con variable del último libro
+	trata(libro);
+};
+
+function trata (libro) {
+
+
+};
